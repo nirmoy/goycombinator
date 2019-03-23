49603e41b8926b49593d284b3213b79318e3b24f
Nirmoy Das <ndas@suse.de>
2019-03-23 01:44:02 +0100

    fetch comment
    Signed-off-by: Nirmoy Das <ndas@suse.de>

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ var (
 	postFmt        = "https://hacker-news.firebaseio.com/v0/item/%v.json"
 	dataCache      map[int]Post
 	dataCacheMutex = sync.RWMutex{}
+	commentMutex   = sync.RWMutex{}
 	postLen        = 0
 )
 
@@ -55,9 +56,94 @@ func fetchUrl(url string, retVal interface{}) (interface{}, error) {
 	return retVal, nil
 }
 
+func fetchPostUrl(url string) (Post, error) {
+	post := Post{}
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return Post{}, err
+	}
+
+	//req.Header.Set("User-Agent", "spacecount-tutorial")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		return Post{}, getErr
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		return Post{}, readErr
+	}
+
+	jsonErr := json.Unmarshal(body, &post)
+	if jsonErr != nil {
+		return Post{}, jsonErr
+	}
+
+	return post, nil
+}
+
+func updateComment(list *widgets.List, kids []int) {
+	list.Rows = []string{}
+	for kid := range kids {
+		commentUrl := fmt.Sprintf(postFmt, kid)
+		go fetchComment(commentUrl, list)
+	}
+}
+
+func fetchComment(url string, list *widgets.List) {
+	comment, _ := fetchCommentUrl(url)
+	if len(comment.Text) > 0 {
+		commentMutex.Lock()
+		list.Rows = append(list.Rows, comment.Text)
+		commentMutex.Unlock()
+	}
+}
+
+func fetchCommentUrl(url string) (Comment, error) {
+	comment := Comment{}
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return Comment{}, err
+	}
+
+	//req.Header.Set("User-Agent", "spacecount-tutorial")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		return Comment{}, getErr
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		return Comment{}, readErr
+	}
+
+	jsonErr := json.Unmarshal(body, &comment)
+	if jsonErr != nil {
+		return Comment{}, jsonErr
+	}
+
+	return comment, nil
+}
+
+type Comment struct {
+	Text string `json:"text"`
+	Url  string `json:url`
+	Kids []int  `json:kids`
+}
 type Post struct {
 	Title string `json:"title"`
-	Url   string `json:url`
+	Url   string `json:"url"`
+	Kids  []int  `json:"kids"`
 }
 
 func openbrowser(url string) {
@@ -75,27 +161,21 @@ func openbrowser(url string) {
 
 }
 func fetchPost(id, postID int) {
+	post := Post{}
 	postUrl := fmt.Sprintf(postFmt, postID)
-	postsVal, err := fetchUrl(postUrl, Post{})
+	post, err := fetchPostUrl(postUrl)
 	if err != nil {
 		return
 	}
-	postStr := postsVal.(map[string]interface{})["title"].(string)
-	if postsVal.(map[string]interface{})["url"] != nil {
-		postUrl = postsVal.(map[string]interface{})["url"].(string)
-		dataCacheMutex.Lock()
-		dataCache[id] = Post{
-			Title: fmt.Sprintf("[%d] %s", id, postStr),
-			Url:   postUrl,
-		}
-		dataCacheMutex.Unlock()
-	}
+	post.Title = fmt.Sprintf("[%v] %s", id, post.Title)
+	dataCacheMutex.Lock()
+	dataCache[id] = post
+	dataCacheMutex.Unlock()
 }
 
 func fetchTopPosts() []int {
 	var posts []interface{}
-	tempVal, err := fetchUrl(topPostUrl, posts)
-	log.Print(err)
+	tempVal, _ := fetchUrl(topPostUrl, posts)
 	postsVal := tempVal.([]interface{})
 	intposts := make([]int, len(postsVal))
 	for i := range postsVal {
@@ -109,6 +189,10 @@ func drawGrid() {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
 	defer ui.Close()
+
+	comment := widgets.NewList()
+	comment.Title = "comments"
+	comment.Rows = []string{}
 
 	l := widgets.NewList()
 	l.Title = "news.ycombinator.com"
@@ -147,6 +231,7 @@ func drawGrid() {
 	grid.Set(
 		ui.NewRow(1.0,
 			ui.NewCol(1.0/2, l),
+			ui.NewCol(1.0/2, comment),
 		),
 	)
 	ui.Render(grid)
@@ -162,8 +247,16 @@ func drawGrid() {
 				return
 			case "j", "<Down>":
 				l.ScrollDown()
+				var id int
+				var title string
+				fmt.Sscanf(l.Rows[l.SelectedRow], "[%d] %s", &id, title)
+				go updateComment(comment, dataCache[id].Kids)
 			case "k", "<Up>":
 				l.ScrollUp()
+				var id int
+				var title string
+				fmt.Sscanf(l.Rows[l.SelectedRow], "[%d] %s", &id, title)
+				go updateComment(comment, dataCache[id].Kids)
 			case "<C-d>":
 				l.ScrollHalfPageDown()
 			case "<C-u>":
